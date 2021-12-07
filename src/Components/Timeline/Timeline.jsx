0ea28cdb56b37527c3a8df201acfb4da395e66db
pedrobando <pedrobandorivera@gmail.com>
2021-12-07 17:59:22 -0600

    Pedro-Create timeline component.

---
+++ src/Components/Timeline/Timeline.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Row, Col, } from "react-bootstrap/";
+import "./Timeline.css";
+
+const Timeline = (props) => {
+    return (
+
+
+      );
+}
+ 
+export default Timeline;
