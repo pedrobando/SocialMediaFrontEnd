811c2d8c1fddb0de89de5f3401685ae4252155ee
pedrobando <pedrobandorivera@gmail.com>
2021-12-08 20:34:35 -0600

    Pedro - Edit form to hold state variables.

--- src/Components/RegistrationPage/RegistrationPage.jsx
+++ src/Components/RegistrationPage/RegistrationPage.jsx
@@ -6,14 +6,33 @@ const RegistrationPage = (props) => {
   const user = props.user;
   const handleSubmit = (event) => {
     event.preventDefault();
-    const form = new FormData();
-    form.append('firstName', event.target[0].value);
-    form.append('lastName', event.target[1].value);
-    form.append('email', event.target[2].value);
-    form.append('christmasPreference', event.target[3].value);
-    form.append('password', event.target[4].value);
-    form.append('dob', event.target[5].value);
-    form.append('image', event.target[6].value);
+    try{
+     
+    let form = new FormData();
+    form.append('firstName', props.value0);
+    form.append('lastName', props.value1);
+    form.append('email', props.value2);
+    form.append('christmasPreference', props.value3);
+    form.append('password', props.value4);
+    form.append('dob', props.value5);
+    form.append('image', props.value6);
+    // const userData = {
+    //   firstName: event.target[0].value,
+    //   lastName: event.target[1].value,
+    //   email: event.target[2].value,
+    //   christmasPreference: event.target[3].value,
+    //   password: event.target[4].value,
+    //   dob: event.target[5].value,
+    //   image: event.target[6].value,
+    // }
+    
+    // Display the key/value pairs
+
+    props.setFormData(form)
+    props.register();
+    }catch(ex){
+      console.log(ex.message)
+    }
     
     const userData = {
       firstName: event.target[0].value,
@@ -24,9 +43,30 @@ const RegistrationPage = (props) => {
       dob: event.target[5].value,
       image: event.target[6].value,
     };
-    console.log(form);
-    props.register(form);
+    
+    
   };
+  const handleChange0 = (event) =>{
+      props.setValue0(event.target.value)
+  }
+  const handleChange1 = (event) =>{
+    props.setValue1(event.target.value)
+}
+const handleChange2 = (event) =>{
+  props.setValue2(event.target.value)
+}
+const handleChange3 = (event) =>{
+  props.setValue3(event.target.value)
+}
+const handleChange4 = (event) =>{
+  props.setValue4(event.target.value)
+}
+const handleChange5 = (event) =>{
+  props.setValue5(event.target.value)
+}
+const handleChange6 = (event) =>{
+  props.setValue6(event.target.value)
+}
 
   return (
     <Row className="center">
@@ -45,46 +85,49 @@ const RegistrationPage = (props) => {
         <>
           <Modal.Dialog>
             <Modal.Body className="mt-5">
-              <Form onSubmit={handleSubmit}>
+              <Form onSubmit={handleSubmit} noValidate >
                 <FloatingLabel
                   controlId="firstName"
                   label="firstName"
                   className="mb-3"
                 >
-                  <Form.Control type="string" placeholder="First Name" />
+                  <Form.Control controlId="value0" onChange={(e) => handleChange0(e)} type="string" placeholder="First Name" />
                 </FloatingLabel>
                 <FloatingLabel
                   controlId="lastName"
                   label="lastName"
                   className="mb-3"
                 >
-                  <Form.Control type="string" placeholder="Last Name" />
+                  <Form.Control controlId="value1" onChange={(e) => handleChange1(e)} type="string" placeholder="Last Name" />
                 </FloatingLabel>
                 <FloatingLabel controlId="email" label="email" className="mb-3">
-                  <Form.Control type="email" placeholder="name@email.com" />
+                  <Form.Control controlId="value2" onChange={(e) => handleChange2(e)} type="string" placeholder="name@email.com" />
                 </FloatingLabel>
                 <FloatingLabel
+                
                   controlId="christmasPreference"
                   label="christmasPreference"
                   className="mb-3"
                 >
                   <Form.Control
+                   controlId="value3"
                     type="string"
                     placeholder="Christmas Preference"
+                    onChange={(e) => handleChange3(e)}
                   />
                 </FloatingLabel>
                 <FloatingLabel
-                  controlId="password"
+                  controlId="value4"
                   label="Password"
                   className="mb-3"
                 >
-                  <Form.Control type="string" placeholder="Password" />
+                  <Form.Control controlId="value5" onChange={(e) => handleChange4(e)} type="string" placeholder="Password" />
                 </FloatingLabel>
                 <FloatingLabel controlId="dob" label="dob" className="mb-3">
-                  <Form.Control type="date" placeholder="Date of Birth" />
+                  <Form.Control controlId="value6" onChange={(e) => handleChange5(e)} type="date" placeholder="Date of Birth" />
                 </FloatingLabel>
                 <Form.Group controlId="image" className="mb-3">
-                  <Form.Control type="file" accept=".jpg,.jpeg,.png"/>
+                  <Form.Control controlId="value7" onChange={(e) => handleChange6(e)} type="file" accept=".jpg,.jpeg,.png"/>
                 </Form.Group>
 
                 <div className="d-grid gap-2">
